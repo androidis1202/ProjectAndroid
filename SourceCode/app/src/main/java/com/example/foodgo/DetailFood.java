6230b6a4757c8166451d244472536b3bfb526485
nguyenanhduc <soitrangtn123@gmail.com>
2019-03-25 00:53:40 +0700

    UPDATED

--- SourceCode/app/src/main/java/com/example/foodgo/DetailFood.java
+++ SourceCode/app/src/main/java/com/example/foodgo/DetailFood.java
@@ -2,12 +2,19 @@ package com.example.foodgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class DetailFood extends AppCompatActivity {
 
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_food);
+        Integer[] number = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this,R.layout.support_simple_spinner_dropdown_item,number);
+        spinner.setAdapter(arrayAdapter);
     }
 }
